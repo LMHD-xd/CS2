eaa97b1a0e8d7e8b766f7376a4d7138110404404
LMHD-xd <luna.henao@Correo.tdea.edu.co>
2024-09-28 15:43:13 -0500

    Delete App (1) directory

--- App (1)/src/main/java/Service/interfaces/AdminService.java
+++
@@ -1,9 +0,0 @@
-
-package Service.interfaces;
-
-import App.Dto.UserDTO;
-
-public interface AdminService {
-    public void createPartner(UserDTO userDTO)throws Exception;
-    public void createGuest (UserDTO userDTO)throws Exception;
-}
